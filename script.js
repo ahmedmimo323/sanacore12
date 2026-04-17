ae74dd39f346d7f4a0dfef725f22bb5718f4f5ee
ahmedmimo323 <ahmedmimo323@gmail.com>
2026-04-17 21:58:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,29 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
+// التبديل بين Sign In و Sign Up
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
+});
+
+// ميزة إظهار وإخفاء كلمة المرور
+document.querySelectorAll('.toggle-password').forEach(icon => {
+    icon.addEventListener('click', function() {
+        const inputId = this.getAttribute('data-target');
+        const input = document.getElementById(inputId);
+        
+        if (input.type === 'password') {
+            input.type = 'text';
+            this.classList.remove('fa-eye');
+            this.classList.add('fa-eye-slash');
+        } else {
+            input.type = 'password';
+            this.classList.remove('fa-eye-slash');
+            this.classList.add('fa-eye');
+        }
+    });
 });
